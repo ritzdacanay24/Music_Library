e5e570f856f62ed4bc3451803e5fa59564931a25
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-07-12 12:00:49 -0700

    Created function to display the data.

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+'use strict';
+
+
 $.ajax({
     method: "GET",
     url: "http://www.devcodecampmusiclibrary.com/api/music",
-    data: {}
+    data: {},
+    async: false,  
   }).done(function( data ) {
-    console.log(data)
+    generateTable(data)
 });
+
+function generateTable(data){
+    let html = '<div>';
+    html = `
+        <tr>
+            <th>Title</th>
+            <th>Album</th>
+            <th>Artist</th>
+            <th>Genre</th>
+            <th>Release Date</th>
+        </tr>
+    `;
+    $.each(data, function( index, value ) {
+        html += `
+            <tr>
+                <td>${value.title}</td>
+                <td>${value.album}</td>
+                <td>${value.artist}</td>
+                <td>${value.genre}</td>
+                <td>${value.releaseDate}</td>
+            </tr>
+        `;
+    });
+    html += '</div>';
+    $('#results').append(html);
+}
+
+function search(){
+    
+}
